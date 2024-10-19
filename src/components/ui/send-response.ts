56e68df66f201178951306244e87bbb2fdef3b28
Yana <annetart1994@gmail.com>
2024-10-19 19:43:23 +0300

    refactor: change sendResponse function for better format

--- src/components/ui/send-response.ts
+++ src/components/ui/send-response.ts
@@ -1,7 +1,14 @@
-import http from 'node:http';
+import * as http from 'node:http';
 
 export const sendResponse = <T>(result: http.ServerResponse, status: number, data: T) => {
+  result.setHeader('Content-Type', 'application/json');
   result.statusCode = status;
-  result.write(JSON.stringify(data));
+
+  if (typeof data === 'string') {
+    result.write(data);
+  } else {
+    result.write(JSON.stringify(data, null, 2));
+  }
+
   result.end();
 };
